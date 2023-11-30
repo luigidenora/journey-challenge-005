4739e9c0532ea836434607ed48bdb82a76c0a0d6
luigidenoraLuigi Denora <denoraluigi24@gmail.com>
2023-11-30 19:40:19 +0000

    chore:  remove allow cors from names.ts

--- challange-server/pages/api/names.ts
+++ challange-server/pages/api/names.ts
@@ -77,20 +77,20 @@ const submitNameHandler = async (req: VercelRequest, res: VercelResponse) => {
   return res.status(405).end(); // Method Not Allowed
 };
 
-const allowCors =
-  (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any }) =>
-  async (req: { method: string }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new (): any; end: { (): void; new (): any } } }) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', (req as any).headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    return await fn(req, res);
-  };
+// const allowCors =
+//   (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any }) =>
+//   async (req: { method: string }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new (): any; end: { (): void; new (): any } } }) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     // another common pattern
+//     // res.setHeader('Access-Control-Allow-Origin', (req as any).headers.origin);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+//     if (req.method === 'OPTIONS') {
+//       res.status(200).end();
+//       return;
+//     }
+//     return await fn(req, res);
+//   };
 
-module.exports = allowCors(submitNameHandler);
+export default submitNameHandler;
