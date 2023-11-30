0d7b13d1b396a8cbb40cd12055b4e953be6eeb40
luigidenoraLuigi Denora <denoraluigi24@gmail.com>
2023-11-30 19:40:19 +0000

    fix: strict error in build

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,8 @@ window.addEventListener('fetchData', ((customEvent: CustomEvent) => {
   const scene = new Scene().add(
     new DirectionalLight('white', 2).translateZ(1),
     new AmbientLight('white', 1),
-    // ...names.map((n: string, i: number) => new Pikachu(n).translateX(3 * Math.random())),
-    // ...names.map((n: string, i: number) => new Pikachu(n).translateX(-3 * Math.random())),
+    ...names.map((n: string, i: number) => new Pikachu(n).translateX(3 * Math.random())),
+    ...names.map((n: string, i: number) => new Pikachu(n).translateX(-3 * Math.random())),
     new Pokeball(camera),
   );
 
