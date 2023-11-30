39a752a59c60540cd50d375ffedd513f6a65a1ea
luigidenoraLuigi Denora <denoraluigi24@gmail.com>
2023-11-30 19:40:18 +0000

    init eyes

--- src/pikachu.ts
+++ src/pikachu.ts
@@ -1,13 +1,6 @@
-import {
-  AnimationAction,
-  AnimationMixer,
-  Group,
-  MeshStandardMaterial,
-  SkinnedMesh,
-  Vector2,
-} from 'three';
+import { AnimationAction, AnimationMixer, Group, MeshStandardMaterial, SkinnedMesh, Vector2 } from 'three';
 import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Main, PerspectiveCameraAuto, Asset, Utils } from '@three.ez/main';
+import { Main, PerspectiveCameraAuto, Asset, Utils, Tween } from '@three.ez/main';
 import { clone } from 'three/examples/jsm/utils/SkeletonUtils';
 
 const GLTF_PICACHU_URL = 'assets/pikachuF.glb';
@@ -28,17 +21,22 @@ export class Pikachu extends Group {
 
     Utils.computeBoundingSphereChildren(this); // to make raycast works properly
 
-    (
-      (this.getObjectByName('PikachuF_4') as SkinnedMesh)
-        .material as MeshStandardMaterial
-    ).map.offset.y = 0.25;
+    const map = ((this.getObjectByName('PikachuF_4') as SkinnedMesh).material as MeshStandardMaterial).map;
+
+    const neutral = new Vector2(0, 0);
+    const opened2 = new Vector2(0, 0.25);
+    const opened3 = new Vector2(0, 0.5);
+    const closed = new Vector2(0, 0.75);
+    const opened4 = new Vector2(0.5, 0);
+    const opened5 = new Vector2(0.5, 0.25);
+    const closed2 = new Vector2(0.5, 0.5);
+
+    const tween = new Tween(map).set({ offset: neutral }).delay(2000).set({ offset: opened2 }).delay(50).set({ offset: opened3 }).delay(100);
+    const tween2 = new Tween(map).then(tween).repeatForever().start();
 
     this.on('animate', (e) => {
       this._mixer.update(e.delta);
-      (
-        (this.getObjectByName('PikachuF_4') as SkinnedMesh)
-          .material as MeshStandardMaterial
-      ).map.offset.y += e.delta % 1;
+      console.log(map.offset);
     });
 
     this.on('pointerenter', (e) => {});
