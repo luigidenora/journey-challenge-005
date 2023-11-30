0f5a927f165a00dbc49e72a0d5222e44a75aae23
luigidenoraLuigi Denora <denoraluigi24@gmail.com>
2023-11-30 19:40:18 +0000

    end Node.js project with Express for handling name submissions.

--- challange-server/pages/api/submit-name.ts
+++ challange-server/pages/api/submit-name.ts
@@ -1,11 +1,22 @@
 // pages/api/submit-name.ts
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import axios from 'axios';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const GIST_ID = 'your-gist-id'; // Replace with your Gist ID
-const GITHUB_TOKEN = 'your-github-token'; // Replace with your GitHub personal access token
+const MONGODB_URI = 'mongodb+srv://user:fW1pg1yJRl150HRR@cluster0.g53y4ss.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB connection string
+const MONGODB_DB = 'cluster0'; // Replace with your MongoDB database name
+const COLLECTION_NAME = 'names';
 
 const submitNameHandler = async (req: VercelRequest, res: VercelResponse) => {
+  console.log('! Received request with method:', req.method);
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(MONGODB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
   if (req.method === 'POST') {
     const { name } = req.body;
 
@@ -13,53 +24,47 @@ const submitNameHandler = async (req: VercelRequest, res: VercelResponse) => {
       return res.status(400).json({ error: 'Name is required' });
     }
 
-    // Check if the name already exists in the Gist
-    if (await nameExistsInGist(name)) {
-      return res.status(400).json({ error: 'Name already exists' });
+    try {
+      // Connect to MongoDB
+      await client.connect();
+
+      // Insert the name into the MongoDB collection
+      const db = client.db(MONGODB_DB);
+      const collection = db.collection(COLLECTION_NAME);
+      await collection.insertOne({ name });
+
+      // Close the MongoDB connection
+      await client.close();
+
+      return res.json({ message: `Name received: ${name}` });
+    } catch (error) {
+      console.error('Error inserting name into MongoDB:', (error as any).message);
+      return res.status(500).json({ error: 'Failed to insert name into MongoDB' });
     }
+  } else if (req.method === 'GET') {
+    try {
+      // Connect to MongoDB
+      await client.connect();
 
-    // Add the name to the Gist
-    await addNameToGist(name);
+      // Retrieve all names from the MongoDB collection
+      const db = client.db(MONGODB_DB);
+      const collection = db.collection(COLLECTION_NAME);
+      const names = await collection.find().toArray();
 
-    return res.json({ message: `Name received: ${name}` });
+      // Close the MongoDB connection
+      await client.close();
+
+      // Extract only the 'name' field from each document
+      const namesArray = names.map((nameDoc) => nameDoc.name);
+
+      return res.json({ names: namesArray });
+    } catch (error) {
+      console.error('Error retrieving names from MongoDB:', (error as any).message);
+      return res.status(500).json({ error: 'Failed to retrieve names from MongoDB' });
+    }
   }
 
   return res.status(405).end(); // Method Not Allowed
 };
-
-// Check if the name already exists in the Gist
-async function nameExistsInGist(name: string): Promise<boolean> {
-  const gistData = await getGistData();
-  return gistData.names.includes(name);
-}
-
-// Add a name to the Gist
-async function addNameToGist(name: string): Promise<void> {
-  const currentData = await getGistData();
-  currentData.names.push(name);
-
-  await axios.patch(
-    `https://api.github.com/gists/${GIST_ID}`,
-    {
-      files: {
-        'data.json': {
-          content: JSON.stringify(currentData),
-        },
-      },
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    }
-  );
-}
-
-// Get the content of the Gist
-async function getGistData(): Promise<{ names: string[] }> {
-  const response = await axios.get(`https://api.github.com/gists/${GIST_ID}`);
-  const dataFile = response.data.files['data.json'];
-  return dataFile ? JSON.parse(dataFile.content) : { names: [] };
-}
 
 export default submitNameHandler;
