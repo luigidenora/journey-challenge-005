ea7c635d45fd01d0fce35edf6fffd6981b65b003
luigidenoraLuigi Denora <denoraluigi24@gmail.com>
2023-11-30 19:40:18 +0000

    chore: fix cors

---
+++ src/dialog.ts
@@ -0,0 +1,81 @@
+// dialog.ts
+export const openDialog = async () => {
+  // Create a dialog container
+  const dialogContainer = document.createElement('div');
+  dialogContainer.classList.add('dialog-container');
+
+  // Create dialog content
+  const dialogContent = document.createElement('div');
+  dialogContent.classList.add('dialog-content');
+
+  // Create input for GitHub username
+  const inputLabel = document.createElement('label');
+  inputLabel.textContent = 'Enter your GitHub username:';
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.value = 'luigidenora'
+  const closeButton = document.createElement('button');
+  closeButton.textContent = 'Close';
+
+  // Add elements to the dialog content
+  dialogContent.appendChild(inputLabel);
+  dialogContent.appendChild(input);
+  dialogContent.appendChild(closeButton);
+
+  // Add dialog content to the container
+  dialogContainer.appendChild(dialogContent);
+
+  // Append the dialog container to the body
+  document.body.appendChild(dialogContainer);
+  closeButton.click();
+  // Add event listener for the close button
+  closeButton.addEventListener('click', async () => {
+    const githubUsername = input.value.trim();
+
+    if (githubUsername) {
+      // If the user provided a username, send it to the server
+      await sendUsernameToServer(githubUsername);
+
+      // After sending, fetch all names from the server
+      const allNames = await fetchAllNames();
+      console.log('All Names:', allNames);
+
+      // Dispatch a custom event with the fetched data
+      const fetchDataEvent = new CustomEvent('fetchData', { detail: allNames });
+      window.dispatchEvent(fetchDataEvent);
+
+      // In a real application, you might want to update your UI with the fetched data
+    }
+
+    // Remove the dialog container from the body
+    document.body.removeChild(dialogContainer);
+  });
+};
+
+const sendUsernameToServer = async (username: string) => {
+  // Send the entered username to the server
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const raw = JSON.stringify({ name: username });
+
+  const requestOptions: RequestInit = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+  // Replace the URL with your actual server URL
+  await fetch('http://localhost:3000/api/names', requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
+};
+
+const fetchAllNames = async () => {
+  // Fetch all names from the server (replace with your actual logic)
+  const response = await fetch('http://localhost:3000/api/names');
+  const data = await response.json();
+  return data.names;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,24 @@
-import {
-  Scene,
-  Mesh,
-  BoxGeometry,
-  MeshNormalMaterial,
-  DirectionalLight,
-  AmbientLight,
-} from 'three';
+import { Scene, Mesh, BoxGeometry, MeshNormalMaterial, DirectionalLight, AmbientLight } from 'three';
 import { Asset, Main, PerspectiveCameraAuto } from '@three.ez/main';
 import { Pikachu } from './pikachu';
-const main = new Main();
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { openDialog } from './dialog';
 
-await Asset.preloadAllPending({
-  onProgress: (e) => console.log(e * 100 + '%'),
+openDialog();
+// const main = new Main();
+// await Asset.preloadAllPending({
+//   onProgress: (e) => console.log(e * 100 + '%'),
+// });
+
+window.addEventListener('fetchData', (event) => {
+  // const scene = new Scene().add(new DirectionalLight('white', 2).translateZ(1), new AmbientLight('white', 1));
+  // const camera = new PerspectiveCameraAuto(70).translateZ(0.5);
+  // const controls = new OrbitControls(camera, main.renderer.domElement);
+  // main.createView({ scene, camera: camera, backgroundColor: 'blue' });
+
+  console.log(event);
+  // for (const name of names) {
+
+  // }
+  //    new Pikachu('luigidenora'),
 });
-
-const scene = new Scene().add(
-  new Pikachu('luigidenora'),
-  new Pikachu('brunosimon').translateX(0.3),
-  new DirectionalLight('white', 2).translateZ(1),
-  new AmbientLight('white', 1)
-  );
-  const camera = new PerspectiveCameraAuto(70).translateZ(0.5);
-  const controls = new OrbitControls(camera, main.renderer.domElement);
-main.createView({ scene, camera: camera, backgroundColor: 'blue' });
